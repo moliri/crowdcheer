1cb474dc79d7a5eb020c6f952cbc125ed40f0bf9
moliri <lmaliakal@u.northwestern.edu>
2014-11-09 17:47:59 -0600

    testing recording

--- hello-world.php
+++ hello-world.php
@@ -3,6 +3,8 @@ header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-<Say>Hello world.</Say>
+	<Say> Press 1 to record </Say>
+	<Gather numDigits="1" action="leave_a_message.php" method="POST"> 
+	</Gather>
 </Response>
 
