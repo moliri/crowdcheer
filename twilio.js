aca22d8796d2b436d10b9520bdc1f037c623b7f9
christinakim <christinahkim1@gmail.com>
2014-11-09 13:24:58 -0600

    fixed functions

--- twilio.js
+++ twilio.js
@@ -16,3 +16,43 @@ client.messages.create({
 });
 
 }
+
+
+function checkSpeed(goal, motivation){
+
+var Speed = Parse.Object.extend("Speed");
+var query = new Parse.Query(Speed);
+
+query.descending("createdAt");
+query.first({
+  success: function(object) {
+    // Successfully retrieved the object.
+    if(object.get('speed') < goal){
+        alert(object.get('speed') + " is less than the goal.")
+        //twilio call
+        var accountSid = 'ACbbb8c6e3d5e297756ff33332f8e5d764'; 
+        var authToken = 'd99dc9a137db2f08404ad65c33c02027'; 
+         
+        //require the Twilio module and create a REST client 
+        var client = require('twilio')(accountSid, authToken); 
+         
+        client.messages.create({ 
+          to: "5712941193", 
+          from: "+12405605233", 
+          body: motivations,   
+        }, function(err, message) { 
+          console.log(message.sid); 
+        });
+
+      }
+      else{
+        alert(object.get('speed') + " is above the goal.")
+
+      }
+
+  },
+  error: function(error) {
+    alert("Error: No speed data yet");
+  }
+});
+}
