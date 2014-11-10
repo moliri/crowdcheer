8d3234dea9cad1e7b47fb675d0c6979cf6ca681d
moliri <lmaliakal@u.northwestern.edu>
2014-11-09 20:08:00 -0600

    moving the saving of the url to after the playback part

--- handle_recording.php
+++ handle_recording.php
@@ -10,23 +10,24 @@ use Parse\ParseObject;
 
 ParseClient::initialize('QXRTROGsVaRn4a3kw4gaFnHGNOsZxXoZ8ULxwZmf', 'BCJuFgG7GVxZfnc2mVbt2dzLz4bP7qAu16xaItXB', 'j9TIxQX3zEHkDPfQszCa6ariYTmZ8JU0RC31BKZK');
 $url = $_REQUEST['RecordingUrl'];
-
-$msgURL = new ParseObject("MsgURL");
-$msgURL->set("messageURL", $url);
-$msgURL->save();
-
-//try {
-  //$msgURL->save();
-  //echo 'New object created with objectId: ' . $msgURL->getObjectId();
-//} catch (ParseException $ex) { 
-  // Execute any logic that should take place if the save fails.
-  // error is a ParseException object with an error code and message.
-  //echo 'Failed to create new object, with error message: ' + $ex->getMessage();
-//}
-
 ?>
+
 <Response>
 <Say>Thanks for the cheer! ... take a listen to what you said.</Say>
 <Play><?php echo $_REQUEST['RecordingUrl']; ?></Play>
 <Say>Goodbye.</Say>
+<?php
+	$msgURL = new ParseObject("MsgURL");
+	$msgURL->set("messageURL", $url);
+	//$msgURL->save();
+
+	try {
+	  $msgURL->save();
+	  echo 'New object created with objectId: ' . $msgURL->getObjectId();
+	} catch (ParseException $ex) { 
+	  // Execute any logic that should take place if the save fails.
+	  // error is a ParseException object with an error code and message.
+	  echo 'Failed to create new object, with error message: ' + $ex->getMessage();
+	}
+?>
 </Response>
--- hello-world.php
+++ hello-world.php
@@ -7,6 +7,8 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	<Record 
 		playBeep="true" 
 		maxLength="30"
+		timeout="3" 
+		finishOnKey="#" 
 		action="handle_recording.php" />
 </Response>
 
