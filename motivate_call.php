ceb31c4fb38d8ac093a146fa6ef95b019401c286
moliri <lmaliakal@u.northwestern.edu>
2014-11-09 19:35:38 -0600

    adding parse info to pull msg URLs into a call to play

--- motivate_call.php
+++ motivate_call.php
@@ -1,6 +1,13 @@
 <?php
 // Include the Twilio PHP library
 require 'twilio-php/Services/Twilio.php';
+require 'vendor/autoload.php';
+//Parse libraries 
+use Parse\ParseClient;
+use Parse\ParseObject;
+use Parse\ParseQuery;
+ParseClient::initialize('QXRTROGsVaRn4a3kw4gaFnHGNOsZxXoZ8ULxwZmf', 'BCJuFgG7GVxZfnc2mVbt2dzLz4bP7qAu16xaItXB', 'j9TIxQX3zEHkDPfQszCa6ariYTmZ8JU0RC31BKZK');
+
 // Twilio REST API version
 $version = "2010-04-01";
 // Set our Account SID and AuthToken
@@ -10,26 +17,23 @@ $token = '9e027d2cffa89f41b75063e1ae9c03b7';
 $phonenumber = '2244124770';
 // Instantiate a new Twilio Rest Client
 $client = new Services_Twilio($sid, $token, $version);
+
+//Parse message URL retrieval	
+$query = new ParseQuery("MsgURL");
+$query->descending("createdAt");
+try {
+	$messageURL = $msgURL->get("messageURL");
+}
+
 try {
 // Initiate a new outbound call
 $call = $client->account->calls->create(
 $phonenumber, // The number of the phone initiating the call
 '8474019260', // The number of the phone receiving call
-
-require 'vendor/autoload.php';
- 
-use Parse\ParseClient;
-use Parse\ParseObject;
-use Parse\ParseQuery;
-
-
- 
-ParseClient::initialize('QXRTROGsVaRn4a3kw4gaFnHGNOsZxXoZ8ULxwZmf', 'BCJuFgG7GVxZfnc2mVbt2dzLz4bP7qAu16xaItXB', 'j9TIxQX3zEHkDPfQszCa6ariYTmZ8JU0RC31BKZK');
-
-
-
-'http://demo.twilio.com/welcome/voice/' // The URL Twilio will request when the call is answered
+$messageURL
+//'http://demo.twilio.com/welcome/voice/' // The URL Twilio will request when the call is answered
 );
+
 echo 'Started call: ' . $call->sid;
 } catch (Exception $e) {
 echo 'Error: ' . $e->getMessage();
