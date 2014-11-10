a58ae72667fcf89dac7de0053ea31b5b604185a0
christinakim <christinahkim1@gmail.com>
2014-11-09 19:01:06 -0600

    added echo

--- speedcheck.php
+++ speedcheck.php
@@ -10,25 +10,32 @@ use Parse\ParseQuery;
  
 ParseClient::initialize('QXRTROGsVaRn4a3kw4gaFnHGNOsZxXoZ8ULxwZmf', 'BCJuFgG7GVxZfnc2mVbt2dzLz4bP7qAu16xaItXB', 'j9TIxQX3zEHkDPfQszCa6ariYTmZ8JU0RC31BKZK');
 
-
+echo("this is the speecheck.php file")
 
 function checkSpeed($goal){
-$justStarted = true;
+  $justStarted = true;
 
-$query = new ParseQuery("Speed");
-$query->descending("createdAt");
-$latestObj = $query->first();
-$latestSpeed = $latestObj->get("speed");
+  $query = new ParseQuery("Speed");
+  $query->descending("createdAt");
+  $latestObj = $query->first();
+  $latestSpeed = $latestObj->get("speed");
+  $latestStatusObj = $latestObj->get("status");
 
-if($latestSpeed >= $goal){
-  $justStarted = false;
-}
+  while(latestStatusObj = "running"){
 
-if($justStarted = false && $latestSpeed){
-  if ($latestSpeed <= $goal){
-    exec("php motivate_call.php");
+    if($latestSpeed >= $goal){
+      $justStarted = false;
+    }
+
+    if($justStarted = false && $latestSpeed){
+      if ($latestSpeed <= $goal){
+        exec("php motivate_call.php");
+      }
+    }
+
+  $latestStatusObj = $statusQuery->first();
   }
-}
+
 
 }
 ?>
