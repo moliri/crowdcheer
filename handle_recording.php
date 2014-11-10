0c099719f48c0e6129604d8591a0fc5d19c9c211
moliri <lmaliakal@u.northwestern.edu>
2014-11-09 19:05:57 -0600

    attempting to store recording URLs in Parse

--- handle_recording.php
+++ handle_recording.php
@@ -3,7 +3,23 @@
 // and play the recording back, using the URL that Twilio posted
 header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+require 'vendor/autoload.php'; 
+use Parse\ParseClient;
+ParseClient::initialize('QXRTROGsVaRn4a3kw4gaFnHGNOsZxXoZ8ULxwZmf', 'BCJuFgG7GVxZfnc2mVbt2dzLz4bP7qAu16xaItXB', 'j9TIxQX3zEHkDPfQszCa6ariYTmZ8JU0RC31BKZK');
 $url = $_REQUEST['RecordingUrl'];
+
+$msgURL = new ParseObject("MsgURL");
+$msgURL->set("messageURL", $url);
+
+try {
+  $msgURL->save();
+  echo 'New object created with objectId: ' . $msgURL->getObjectId();
+} catch (ParseException $ex) { 
+  // Execute any logic that should take place if the save fails.
+  // error is a ParseException object with an error code and message.
+  echo 'Failed to create new object, with error message: ' + $ex->getMessage();
+}
+
 ?>
 <Response>
 <Say>Thanks for the cheer! ... take a listen to what you said.</Say>
--- hello-world.php
+++ hello-world.php
@@ -3,7 +3,11 @@ header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-	<Say> Record your 5 second motivational message now. </Say>
-	<Record maxLength="5" action="handle_recording.php" />
+	<Say> Record your motivational message now. Press # when you are finished. </Say>
+	<Record 
+		playBeep="true" 
+		maxLength="30" 
+		finishOnKey="#" 
+		action="handle_recording.php" />
 </Response>
 
